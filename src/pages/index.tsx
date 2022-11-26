d77956f54569ac4f68390d7858b2e94020829de7
Pavel yatskevich <Pavel.Yatskevitch@shell.com>
2022-11-26 21:07:23 +0100

    trpc init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,16 @@
+import { trpc } from "@/utils/trpc";
+
 export default function Home() {
+  const hello = trpc.hello.useQuery({ text: "client" });
+  if (!hello.data) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      <p>{hello.data.greeting}</p>
+    </div>
+  );
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center align-middle items-center">
       <div className="text-2xl text-center">Which Pokemon is Rounded?</div>
@@ -8,6 +20,8 @@ export default function Home() {
         <div>vs</div>
         <div className="w-16 h-16 bg-red-200"></div>
       </div>
+
+      <div>{data}</div>
     </div>
   );
 }
