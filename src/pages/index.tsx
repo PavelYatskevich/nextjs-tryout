5d3711d0d84ab1f4f4d87137e001f456d4f41a96
Pavel yatskevich <Pavel.Yatskevitch@shell.com>
2022-11-26 21:25:39 +0100

    add random pokemons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,18 @@
+import { getOptionsForVote } from "@/utils/getRandomPokemom";
 import { trpc } from "@/utils/trpc";
 
 export default function Home() {
-  const hello = trpc.hello.useQuery({ text: "client" });
-  if (!hello.data) {
-    return <div>Loading...</div>;
-  }
-  return (
-    <div>
-      <p>{hello.data.greeting}</p>
-    </div>
-  );
+  const [firstId, secondId] = getOptionsForVote();
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center align-middle items-center">
       <div className="text-2xl text-center">Which Pokemon is Rounded?</div>
       <div className="p-2"></div>
       <div className="border rounded p-8 flex justify-between max-w-2xl ">
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-200">{firstId}</div>
         <div>vs</div>
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-200">{secondId}</div>
       </div>
-
-      <div>{data}</div>
     </div>
   );
 }
---
+++ src/utils/getRandomPokemom.ts
@@ -0,0 +1,16 @@
+const MAX_DEX_ID = 493
+
+export const getRandomPokemom = (notThisOne?: number): number => {
+    const pokedexNumber = Math.floor(Math.random() * (MAX_DEX_ID)) + 1
+
+    if(pokedexNumber !==  notThisOne) return pokedexNumber
+
+    return getRandomPokemom(notThisOne)
+}
+
+export const getOptionsForVote = () => {
+    const firstId = getRandomPokemom();
+    const secondId = getRandomPokemom(firstId);
+
+    return [firstId, secondId]
+}
