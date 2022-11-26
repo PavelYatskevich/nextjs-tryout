b81e04707e6881b8e172ad0b2468db77350c2b9c
Pavel yatskevich <Pavel.Yatskevitch@shell.com>
2022-11-26 20:06:22 +0100

    basic UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center align-middle items-center">
+      <div className="text-2xl text-center">Which Pokemon is Rounded?</div>
+      <div className="p-2"></div>
+      <div className="border rounded p-8 flex justify-between max-w-2xl ">
+        <div className="w-16 h-16 bg-red-200"></div>
+        <div>vs</div>
+        <div className="w-16 h-16 bg-red-200"></div>
+      </div>
+    </div>
+  );
 }
